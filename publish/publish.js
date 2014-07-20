8f8a6ea7e09bc63b10522c3f243bff881afadcf9
Philip Hofstetter <phofstetter@sensational.ch>
2014-07-20 15:57:34 +0200

    add event-published
    this is the generic script we're going to use to push events for most of
    the demo code.

---
+++ publish/publish.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+var sys = require('sys');
+
+var redis = require('redis');
+var pg = require("pg").native;
+
+pg.defaults.poolSize = 2
+
+
+var cheese_types = ['Emmentaler', 'Appenzeller', 'Gruyère',
+    'Vacherin', 'Sprinz'];
+
+
+function create_cheese(){
+    return {
+      pieces: Math.floor(Math.random() * 115) + 5,
+      type: cheese_types[Math.floor(Math.random()*cheese_types.length)]
+    }
+}
+
+
+var cheese_delivery = create_cheese();
+publish(cheese_delivery);
+
+
+
+function publish(cheese_event){
+  query(
+    "insert into events (channel_id, data) values ($1, $2)",
+    ['cheese', JSON.stringify(cheese_event)],
+    function(err, rows){
+      redis_publish('cheese', cheese_event, function(err, res){
+        console.log(
+          "Created " + cheese_delivery.pieces + " pieces of " + cheese_delivery.type
+        );
+      })
+    }
+  );
+}
+
+function with_postgres(cb){
+  pg.connect('postgres://pilif@localhost/cheese', function(err, client){
+    cb(err, client);
+  });
+}
+
+function query(query, params, cb){
+  with_postgres(function(err, client){
+    if (err) return cb(err, null);
+    client.query(query, params, function(err, res){
+      if (err) return cb(err, null);
+      cb(null, res.rows);
+      client.end();
+    });
+  });
+}
+
+function with_redis(cb){
+  var client = redis.createClient(6379, 'localhost');
+  client.select(2, function(err, result){
+    if (err) return cb(err, null);
+    cb(null, client);
+  })
+}
+
+function redis_publish(channel, data, cb){
+  with_redis(function(err, client){
+    if (err) return cb(err, null);
+    client.publish('channels:cheese', JSON.stringify(data), function(err, result){
+      client.end();
+      cb(err, result);
+    });
+  });
+}
