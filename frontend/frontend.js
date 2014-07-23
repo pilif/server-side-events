0ad507b537ce07ef209f8964884e72c9895b8d79
Philip Hofstetter <phofstetter@sensational.ch>
2014-07-23 09:48:48 +0200

    add reverse proxy support to our demo server
    CORS headers don't go well with long-polling and because the things run
    on different ports, they are seen as being cross-origin, so let's just
    proxy the connection to the long-polling server

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,13 +1,27 @@
 #!/usr/bin/env node
 var http = require('http');
 var serveStatic = require('serve-static');
+var httpProxy = require('http-proxy');
 var finalhandler = require('finalhandler')
 
 // Serve up public/ftp folder
 var serve = serveStatic('htdocs', {'index': ['index.html']});
 
+var proxy = httpProxy.createProxyServer({
+    target: 'http://localhost:9984'
+});
+
 // Create server
 var server = http.createServer(function(req, res){
+  if (req.url.match(/^\/events/)){
+    req.url = req.url.substr(7);
+    return proxy.web(req, res, function(err, proxy_result){
+        if (err){
+            res.writeHead(504, {'Content-Type:': 'text/plain'});
+            res.end('proxy error');
+        }
+    });
+  }
   serve(req, res, finalhandler(req, res));
 })
 
